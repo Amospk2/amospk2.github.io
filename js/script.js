32260348e427725156247c52fae3ce81cc131b44
Amospk2 <38811989+Amospk2@users.noreply.github.com>
2020-04-30 00:55:43 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function change(){
-	var MinhaDiv = document.getElementById("MinhaDiv");
-	MinhaDiv.classList.add("meuestilo");
-	
-	var Aga2 = document.getElementById("aga2");
-	Aga2.innerHTML = "Teste 0001";
-	
+(function (window, document) {
 
-}
+})(window, document);
