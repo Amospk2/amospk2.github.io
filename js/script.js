322223e3532d121044463efbc7756e458e964301
Amospk2 <38811989+Amospk2@users.noreply.github.com>
2020-05-02 00:21:25 -0300

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#List dd").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
