8d5ce03ea52b3ad34dfc2f0d433a4b9af65fa09d
Amospk2 <38811989+Amospk2@users.noreply.github.com>
2020-05-02 00:26:05 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#List dd").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
